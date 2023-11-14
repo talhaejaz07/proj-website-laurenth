3b504a363e978fd619f3997bfaeecce0f356d465
Talha Ejaz <talhaejaz@outlook.com>
2023-11-15 02:20:12 +0500

    tabs done

---
+++ src/data/tabsData.js
@@ -0,0 +1,19 @@
+export const stepDescriptions = {
+  1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at metus vulputate, pellentesque neque vel, facilisis lorem. Phasellus id odio sed lorem tincidunt vulputate nec eget odio.",
+  2: "Quisque quis dui ullamcorper mauris dictum dictum finibus ac leo. Nunc mattis dignissim ex, eu malesuada metus lobortis posuere. Suspendisse mollis interdum lorem.",
+  3: "Nam cursus laoreet quam eget blandit. Fusce aliquet turpis a enim malesuada ultrices. Vestibulum eu fermentum nulla, ut mattis magna. Suspendisse consequat, nunc facilisis dictum vehicula.",
+  4: "Donec libero mauris, consequat sit amet lacus ac, elementum rutrum nulla. Proin semper, tellus vel cursus auctor, purus ante condimentum felis, id facilisis leo tellus a est.",
+  5: "Mauris at aliquam dui. Aliquam aliquet dignissim hendrerit. In rhoncus libero massa, vitae suscipit dolor volutpat sodales. Curabitur ut erat sed libero egestas condimentum a ac neque. Mauris gravida tellus sit amet laoreet tempus.",
+  6: "Nam sit amet rutrum massa. Aliquam a felis id risus faucibus pharetra. Sed dui odio, aliquam ac maximus in, dictum quis metus. Vestibulum ligula ante.",
+  7: "Pellentesque vestibulum magna et risus efficitur, ut faucibus ante vestibulum. Aliquam erat volutpat. Etiam varius, nibh quis sagittis laoreet, purus nisi scelerisque.",
+};
+
+export const stepColors = {
+  1: "#E8D24A",
+  2: "#D38FA5",
+  3: "#B55C78",
+  4: "#B5DBCC",
+  5: "#8DB6A6",
+  6: "#71B1C6",
+  7: "#007BA4",
+};
