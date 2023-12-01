cf9143cb53bcaec21a2b3cbe3e50d6f8f787353f
Talha Ejaz <talhaejaz@outlook.com>
2023-12-01 08:49:16 +0500

    blog page done

--- src/content/config.js
+++ src/content/config.js
@@ -28,8 +28,24 @@ const stepsCollection = defineCollection({
   }),
 });
 
+const blogPostsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string(),
+      title: z.string(),
+      description: z.string(),
+      datePublished: z.date(),
+      dateUpdated: z.date(),
+      tags: z.array(z.string()),
+      imageUrl: image(),
+      imageAlt: z.string(),
+    }),
+});
+
 export const collections = {
   profiles: profilesCollection,
   testimonials: testimonialsCollection,
   steps: stepsCollection,
+  blogPosts: blogPostsCollection,
 };
